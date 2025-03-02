e6e20bd211691e10a5482c91474012333cf50778
kodingkin <wankin527@gmail.com>
2025-03-02 12:33:19 +0900

    finish 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let para = ""
+    if (num <0) {
+        para += 'ERROR';
+    }
+    for (let i = 0; i<num; i++) {
+        para += str;
+    }
+    return para;
 };
 
 // Do not edit below this line
